e436dd8da7e67ecd36f595b34025d63428b0b898
gvolsky <47405015+gvolsky@users.noreply.github.com>
2022-12-18 23:58:07 +0300

    Update simple_converter.py

--- Задание 2/simple_converter.py
+++ Задание 2/simple_converter.py
@@ -1,16 +1,15 @@
 import sys
+
 def read_data(file_name):
     file = open(file_name)
     content = file.read()
     file.close()
     return content
 
-
 def write_data(file_name, data):
     file = open(file_name, 'w')
     file.write(data)
     file.close()
-
 
 def read_data_to_list(file_name):
     file = open(file_name)
@@ -18,14 +17,12 @@ def read_data_to_list(file_name):
     file.close()
     return content
 
-
 def prepare_data(data):
     if data:
         title = data.pop(0).strip().split(',')
     else:
         title = []
     return title, data
-
 
 def convert_row_to_pretty_json(titles, row):
     values = row.strip().split(',')
@@ -41,7 +38,6 @@ def convert_row_to_pretty_json(titles, row):
   }}""".format(*raw_data)
     return data
 
-
 def convert_csv_to_json(data):
     titles, data = prepare_data(data)
     result = []
@@ -49,7 +45,6 @@ def convert_csv_to_json(data):
         result.append(convert_row_to_pretty_json(titles, row))
     result = ''.join(['[\n', ',\n'.join(result), '\n]'])
     return result
-
 
 def main():
     inp, out = sys.argv[1:3]
