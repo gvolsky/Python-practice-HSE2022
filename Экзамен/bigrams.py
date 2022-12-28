4ef2f2d064de6647bfef388a35296e41966eafe7
Artemy Moseichuk <47405015+gvolsky@users.noreply.github.com>
2022-12-28 18:22:48 +0300

    Update bigrams.py

--- Экзамен/bigrams.py
+++ Экзамен/bigrams.py
@@ -46,11 +46,12 @@ def count_bigrams(data):
     for line in data:
         if len(line) == 1:
             words[line[0]] += 1
-            count_letter(line[0], letters)                
-        for idx in range(len(line) - 1):
-            words[' '.join([line[idx], line[idx + 1]])] += 1
-            count_letter(line[idx], letters) 
-            count_letter(line[idx + 1], letters) 
+            count_letter(line[0], letters)     
+        else:
+            for idx in range(len(line) - 1):
+                words[' '.join([line[idx], line[idx + 1]])] += 1
+                count_letter(line[idx], letters) 
+                count_letter(line[idx + 1], letters) 
     return words, letters
 
 def main():
